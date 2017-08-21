acad6d94c12642c459a430fd7483d7fd9fa3f6e5
Aleisamo <moreno.isabel.isabel@gmail.com>
2017-08-21 23:34:47 +0100

    Review styles Remove hardcoded values from layouts

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -246,7 +246,6 @@ public class ArticleDetailFragment extends Fragment implements
                                 mMutedColor = p.getDarkMutedColor(0xFF333333);
                                 mPhotoView.setImageBitmap(imageContainer.getBitmap());
                                 mRootView.findViewById(R.id.meta_bar);
-                                        //.setBackgroundColor(mMutedColor);
                                 updateStatusBar();
                             }
                         }
